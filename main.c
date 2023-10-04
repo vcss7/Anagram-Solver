1f66b1454700c9e899b44f37ea2a739cd83c4337
vcss7 <vs.victorsolis@gmail.com>
2023-10-04 01:28:56 -0700

    messy find word in file

--- main.c
+++ main.c
@@ -1,25 +1,86 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool verify_word(char** word);
+const uint8_t MAX_WORD_LENGTH = 20;
+
+bool is_word_in_words_file(char* word, FILE* words_file);
 
 int main(int argc, char** argv)
 {
-    char word[20];
+    /* Open words file */
+    FILE* words_file = NULL;
+    // TODO: Check for debian-based distributions
+    words_file = fopen("/usr/share/dict/words", "r");
 
-    printf("\nAnagram Solver");
-    printf("\nEnter a word > ");
-    scanf("%s", &word);
+    if(words_file == NULL)
+    {
+        perror("\nWas not able to open file.\n");
+        return 1;
+    }
 
-    printf("\nChecking anagrams for %s..\n", &word);
 
+    /* Run anagram solver */
+    char word[MAX_WORD_LENGTH];
+
+    printf("\n===== Anagram Solver =====\n");
+    bool read_input = false;
+    while(!read_input)
+    {
+        printf("\nEnter a word > ");
+        if(scanf("%s", &word) != 1)
+        {
+            printf("Error reading input.\n");
+            continue;
+        }
+
+        if(!is_word_in_words_file(word, words_file))
+        {
+            printf("\n%s is NOT a valid word. Try again.\n", word);
+            continue;
+        }
+
+        read_input = true;
+    }
+
+
+    /* Clean up / End program */
+    fclose(words_file);
     return 0;
 }
 
-bool verify_word(char** word)
+bool is_word_in_words_file(char* word, FILE* words_file)
 {
-    // TODO: check if word is in dictionary
+    // Search for a word in a file with word delimited by a new line
+    // Case sensitive
+    // Assumes valid FILE pointer
 
-    return true;
+    char buffer[MAX_WORD_LENGTH];
+
+    while(fgets(buffer, sizeof buffer, words_file) != NULL)
+    {
+        // remove new line character
+        size_t length = strlen(buffer);
+        if(length > 0 && buffer[length - 1] == '\n')
+        {
+            buffer[length - 1] = '\0';
+        }
+
+        if(strcmp(word, buffer) == 0)
+        {
+            rewind(words_file);
+            return true;
+        }
+    }
+
+    // check for EOF status
+    if(!feof(words_file))
+    {
+        perror("Error reading file.\n");
+    }
+
+    rewind(words_file);
+    return false;
 }
 
